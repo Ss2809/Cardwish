fe64032fe5821c58c1b0ed3d9e4070e64d47be05
Sushil <sushilpawar2321@gmail.com>
2026-01-04 19:28:01 +0530

    order_db_done

---
+++ models/order.js
@@ -0,0 +1,32 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true, min: 1, default: 1 },
+        title: { type: String, required: true },
+        price: { type: Number, required: true },
+        image: { type: String, required: true },
+       
+      },
+    ],
+    totalProduct: { type: Number, default: 0 },
+    totalPrice: { type: Number, default: 0 },
+    shippingAddress : {type:String,required : true},
+    paymentId : {type: String, required : true},
+    paymentStatus : {type:String,required : true},
+    orderStatus : {type:String,enum : ["pending","processing","shipped","delivered","cancelled"], default : "pending"},
+    creatAt :{type:Date, default : Date.now()},
+    deliveredAt : {type:Date}
+
+})
+
+const Order = mongoose.model("Order",orderSchema);
+module.exports = Order;
--- routes/card.js
+++ routes/card.js
@@ -56,4 +56,105 @@ router.post("/:id", authmiddleware, async (req, res) => {
   res.status(201).json({ message: "Product added to cart", card });
 });
 
+router.get("/", authmiddleware, async (req, res) => {
+  const userId = req.user._id;
+  const cardData = await Card.findOne({ user: userId });
+  if (!cardData) {
+    res.status(404).json({ message: "User card is Empty!!" });
+  }
+  res.status(200).json(cardData);
+});
+
+router.patch("/increase/:productId", authmiddleware, async (req, res) => {
+  const productId = req.params.productId;
+  const userId = req.user._id;
+  const product = await Product.findById(productId);
+  if (!product) {
+    return res.json({ message: "invalid product!" });
+  }
+  const card = await Card.findOne({ user: userId });
+  if (!card) {
+    res.json({ message: "card not avaibale" });
+  }
+  const index = card.products.findIndex(
+    (p) => p.productId.toString() === productId
+  );
+  if (index === -1) {
+    return res.json({ message: "invalid index or product !!" });
+  }
+  if (card.products[index].quantity >= product.stock) {
+    return res.json({ message: "not increase by 1 || stock is end" });
+  }
+  card.products[index].quantity += 1;
+  card.totalProduct += 1;
+  card.totalCardPrice += product.price;
+
+  await card.save();
+  res.json({ message: "update succfully!", card });
+});
+router.patch("/decrease/:productId", authmiddleware, async (req, res) => {
+  const productId = req.params.productId;
+  const userId = req.user._id;
+  const product = await Product.findById(productId);
+  if (!product) {
+    return res.json({ message: "invalid product!" });
+  }
+  const card = await Card.findOne({ user: userId });
+  if (!card) {
+    res.json({ message: "card not avaibale" });
+  }
+  const index = card.products.findIndex(
+    (p) => p.productId.toString() === productId
+  );
+  if (index === -1) {
+    return res.json({ message: "invalid index or product !!" });
+  }
+  if (card.products[index].quantity > 1) {
+    card.products[index].quantity -= 1;
+  } else {
+    card.products.splice(index, 1);
+  }
+
+  card.totalProduct -= 1;
+  card.totalCardPrice -= product.price;
+
+  await card.save();
+  res.json({ message: "update succfully!", card });
+});
+router.patch("/remove/:productId", authmiddleware, async (req, res) => {
+  const productId = req.params.productId;
+  const userId = req.user._id;
+  const product = await Product.findById(productId);
+  if (!product) {
+    return res.json({ message: "invalid product!" });
+  }
+  const card = await Card.findOne({ user: userId });
+  if (!card) {
+    return res.json({ message: "card not avaibale" });
+  }
+  const index = card.products.findIndex(
+    (p) => p.productId.toString() === productId
+  );
+  if (index === -1) {
+    return res.json({ message: "invalid index or product !!" });
+  }
+
+  if (
+    card.products.length === 1 &&
+    card.products[index].productId.toString() === productId
+  ) {
+    await Card.findByIdAndDelete(card._id);
+    return res.json({ message: "card are empty!!" });
+  }
+  const removedProduct = card.products[index];
+
+  card.totalProduct -= removedProduct.quantity;
+  card.totalCardPrice -= removedProduct.quantity * removedProduct.price;
+
+  card.products.splice(index, 1);
+
+  await card.save();
+  res.json({ message: "Remove Succfully!!", card });
+});
+
 module.exports = router;
